3f8bf2e2fc23cac5b7405a8f3fa599cbae03e714
Wahyudi Prasidhatama <wahyudiprasidhatama@gmail.com>
2023-07-04 14:12:58 +0700

    update

--- flinkstreamingkafka/flink-kafka-streaming/src/main/java/com/prsdhatama/flinkkafkastreaming/testconnection/TestConnection.java
+++ flinkstreamingkafka/flink-kafka-streaming/src/main/java/com/prsdhatama/flinkkafkastreaming/testconnection/TestConnection.java
@@ -1,12 +1,14 @@
 package com.prsdhatama.flinkkafkastreaming.testconnection;
 
-        import org.apache.flink.api.common.serialization.SimpleStringSchema;
-        import org.apache.flink.configuration.Configuration;
-        import org.apache.flink.streaming.api.datastream.DataStream;
-        import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-        import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-        import java.util.Properties;
+import java.util.Properties;
 
 public class TestConnection {
     public static void main(String[] args) throws Exception {
@@ -14,9 +16,9 @@ public class TestConnection {
                 .createLocalEnvironmentWithWebUI(new Configuration());
 
         //Set up Kafka properties
-        Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "localhost:9092");
-        properties.setProperty("group.id", "test-group");
+//        Properties properties = new Properties();
+//        properties.setProperty("bootstrap.servers", "localhost:9092");
+//        properties.setProperty("group.id", "test-group");
 
 //        Properties properties = new Properties();
 //        properties.setProperty("bootstrap.servers", "localhost:9092");
@@ -24,18 +26,47 @@ public class TestConnection {
 //        properties.setProperty("topic", "animalia");
 //        properties.setProperty("group.id", "test-group");
 //        properties.setProperty("auto.offset.reset", "earliest");
+        KafkaSource<String> source = KafkaSource.<String>builder()
+                .setBootstrapServers("localhost:9092")
+                .setTopics("quickstart-events")
+                .setGroupId("my-group")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+
+
+//        KafkaSource<String> kafkaSource = KafkaSource
+//                .<String>builder()
+//                .setBootstrapServers("localhost:9092")
+//                .setTopics("animalia")
+//                .setDeserializer(new SimpleStringSchema())
+//                .setProperties(properties)
+//                .build();
+
 
         // Create a Kafka consumer
-        FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<>(
-                "animalia",
-                new SimpleStringSchema(),
-                properties
-        );
+//        KafkaSource<SchemaDictionary> dictionaryKafkaSource = KafkaSource
+//                .<SchemaDictionary>builder()
+//                .setBootstrapServers("localhost:9092") //localhost
+//                .setTopics("SchemaDictionary")
+//                .setValueOnlyDeserializer(new Deserialization<SchemaDictionary>(SchemaDictionary.class))
+//                .setStartingOffsets(OffsetsInitializer.earliest())
+//                .setGroupId("flink-yudi")
+//                .build();
+
+
+//        FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<>(
+//                "animalia",
+//                new SimpleStringSchema(),
+//                properties
+//        );
+        DataStream<String> kafkaDataStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 
         // Add the Kafka consumer as a source to the Flink environment
-        DataStream<String> kafkaDataStream = env.addSource(kafkaConsumer);
+//        DataStream<String> kafkaDataStream = env.addSource(kafkaConsumer);
 
         // Print the Kafka messages to the console
+//        kafkaDataStream.print();
         kafkaDataStream.print();
 
         // Execute the Flink job
