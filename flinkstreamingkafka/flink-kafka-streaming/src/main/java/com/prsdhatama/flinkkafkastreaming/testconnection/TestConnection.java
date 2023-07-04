a3df4713c1b431dbb7349b36decc568619927395
Wahyudi Prasidhatama <wahyudiprasidhatama@gmail.com>
2023-07-04 14:22:11 +0700

    update

--- flinkstreamingkafka/flink-kafka-streaming/src/main/java/com/prsdhatama/flinkkafkastreaming/testconnection/TestConnection.java
+++ flinkstreamingkafka/flink-kafka-streaming/src/main/java/com/prsdhatama/flinkkafkastreaming/testconnection/TestConnection.java
@@ -8,24 +8,11 @@ import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsIni
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-import java.util.Properties;
-
 public class TestConnection {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment
                 .createLocalEnvironmentWithWebUI(new Configuration());
 
-        //Set up Kafka properties
-//        Properties properties = new Properties();
-//        properties.setProperty("bootstrap.servers", "localhost:9092");
-//        properties.setProperty("group.id", "test-group");
-
-//        Properties properties = new Properties();
-//        properties.setProperty("bootstrap.servers", "localhost:9092");
-//        properties.setProperty("zookeeper.connect", "localhost:2181"); // Zookeeper default host:port
-//        properties.setProperty("topic", "animalia");
-//        properties.setProperty("group.id", "test-group");
-//        properties.setProperty("auto.offset.reset", "earliest");
         KafkaSource<String> source = KafkaSource.<String>builder()
                 .setBootstrapServers("localhost:9092")
                 .setTopics("quickstart-events")
@@ -34,42 +21,10 @@ public class TestConnection {
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
 
-
-//        KafkaSource<String> kafkaSource = KafkaSource
-//                .<String>builder()
-//                .setBootstrapServers("localhost:9092")
-//                .setTopics("animalia")
-//                .setDeserializer(new SimpleStringSchema())
-//                .setProperties(properties)
-//                .build();
-
-
-        // Create a Kafka consumer
-//        KafkaSource<SchemaDictionary> dictionaryKafkaSource = KafkaSource
-//                .<SchemaDictionary>builder()
-//                .setBootstrapServers("localhost:9092") //localhost
-//                .setTopics("SchemaDictionary")
-//                .setValueOnlyDeserializer(new Deserialization<SchemaDictionary>(SchemaDictionary.class))
-//                .setStartingOffsets(OffsetsInitializer.earliest())
-//                .setGroupId("flink-yudi")
-//                .build();
-
-
-//        FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<>(
-//                "animalia",
-//                new SimpleStringSchema(),
-//                properties
-//        );
         DataStream<String> kafkaDataStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 
-        // Add the Kafka consumer as a source to the Flink environment
-//        DataStream<String> kafkaDataStream = env.addSource(kafkaConsumer);
-
-        // Print the Kafka messages to the console
-//        kafkaDataStream.print();
         kafkaDataStream.print();
 
-        // Execute the Flink job
         env.execute("Kafka Connectivity Test");
     }
 }
