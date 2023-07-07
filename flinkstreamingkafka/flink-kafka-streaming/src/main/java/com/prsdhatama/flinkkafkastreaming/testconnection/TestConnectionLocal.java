ec2f744d36564fdafc1ed50a4b239f7c81632f25
Wahyudi Prasidhatama <wahyudiprasidhatama@gmail.com>
2023-07-07 15:49:51 +0700

    update

--- flinkstreamingkafka/flink-kafka-streaming/src/main/java/com/prsdhatama/flinkkafkastreaming/testconnection/TestConnectionLocal.java
+++ flinkstreamingkafka/flink-kafka-streaming/src/main/java/com/prsdhatama/flinkkafkastreaming/testconnection/TestConnectionLocal.java
@@ -8,7 +8,7 @@ import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsIni
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-public class TestConnection {
+public class TestConnectionLocal {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment
                 .createLocalEnvironmentWithWebUI(new Configuration());
